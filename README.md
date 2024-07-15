aedc39058cc4a76f6edc2b561a1801184a2fb5c9
RealTinyWarrior <sincpls@gmail.com>
2024-07-15 17:09:48 +0600

    Trying LazyLoading css for Recent

--- README.md
+++ README.md
@@ -5,7 +5,9 @@ The code of the official website of TinyWarrior, TinyTweaks! If you are interest
 The Code:
 
 -   In the App directory, we have the routes
--   In the Components directory, we have the components used in this project
--   In the Image and Public directory, we have Images and CSS Files respectively
+-   In the Components directory, we have the components used in this portfolio
+-   In the Image directory, we have the images used in this portfolio
+-   In the Styles directory, we have all of the css files
+-   In the Root, we have data.ts which contains some of the datas of this project
 
 Drop a star if you like it :3
--- components/HomeRecent.tsx
+++ components/HomeRecent.tsx
@@ -67,7 +67,10 @@ const HomeRecent = () => {
                     <div className="slides_inner">
                         {recent.map((elm) => (
                             <div className="recent_slide" key={elm.name}>
-                                <div className={style.background_image_recent}>
+                                <div
+                                    className={style.background_image_recent}
+                                    style={{ backgroundColor: elm.lazyloading }}
+                                >
                                     <Image className={style.fit_image} src={elm.background} alt={elm.name} />
                                 </div>
 
