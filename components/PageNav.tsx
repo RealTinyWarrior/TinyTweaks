643bc3dea8868eb0f17981b583de6e39a0ba0703
TinyWarrior <tahmidradid@gmail.com>
2025-01-24 14:40:29 +0600

    Added "?" on navs

--- components/PageNav.tsx
+++ components/PageNav.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import style from "@styles/pagenav.module.css";
 import PageNavButton from "@components/PageNavButton";
 import { FaUser } from "react-icons/fa";
+import { FaQuestionCircle } from "react-icons/fa";
 
 const PageNav = () => {
     return (
         <div id={style.container}>
             <PageNavButton name="About" icon={<FaUser />} />
-            <PageNavButton name="Blank" icon={<FaUser />} />
-            <PageNavButton name="Blank" icon={<FaUser />} />
-            <PageNavButton name="Blank" icon={<FaUser />} />
+            <PageNavButton name="Blank" icon={<FaQuestionCircle />} />
+            <PageNavButton name="Blank" icon={<FaQuestionCircle />} />
+            <PageNavButton name="Blank" icon={<FaQuestionCircle />} />
         </div>
     );
 };
